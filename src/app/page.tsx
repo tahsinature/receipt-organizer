117c5c8bf3ac08ca3ea998e358569d681022e00e
Tahsin <work.tahsin@gmail.com>
2024-06-02 19:40:03 -0400

    add fileinput

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,12 @@
 import CameraInput from "@/components/CameraInput";
+import FileInput from "@/components/FileInput";
 
 export default function Home() {
   return (
     <main>
       <CameraInput />
 
-      <div>
-        <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Upload multiple files</label>
-        {/* <input className="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" id="multiple_files" type="file" multiple> */}
-      </div>
+      <FileInput />
     </main>
   );
 }
