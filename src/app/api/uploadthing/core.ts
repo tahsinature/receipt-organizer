c245a7adb2c14fd624964e938bc6a14c66c5487b
Tahsin <work.tahsin@gmail.com>
2024-06-08 16:17:14 -0400

    add uploadthing api

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -1,18 +1,43 @@
 import { extractReceiptInfo } from "@/server/ai";
+import { saveReceipt } from "@/server/notion";
+import { SaveReceiptSchema } from "@/server/schema";
+import { validateWithZod } from "@/server/validation";
+import { ReceiptDataFromAI } from "@/types";
 import { createUploadthing, type FileRouter } from "uploadthing/next";
 
 const f = createUploadthing();
 
-const auth = (req: Request) => ({ id: "fakeId" }); // Fake auth function
-
 export const ourFileRouter = {
   imageUploader: f({ image: { maxFileSize: "16MB" } }).onUploadComplete(async ({ metadata, file }) => {
-    // This code RUNS ON YOUR SERVER after upload
-    const fileURL = file.url;
+    let error = null;
+    let data: { notionURL: string; notionID: string; imageURL: string; fileKey: string; receiptData: ReceiptDataFromAI } | null = null;
 
-    const aiData = await extractReceiptInfo(fileURL);
+    try {
+      const imageURL = file.url;
+      const fileKey = file.key;
 
-    return aiData;
+      const receiptData = await extractReceiptInfo(imageURL);
+
+      const dataForNotion = {
+        amount: receiptData.amount_spend,
+        date: receiptData.date,
+        category: receiptData.category,
+        items: receiptData.items,
+        store: receiptData.store_name,
+        title: receiptData.title,
+      };
+
+      const { error: zodError, parsedData } = validateWithZod(dataForNotion, SaveReceiptSchema);
+      if (zodError) throw zodError;
+
+      const { url: notionURL, id: notionID } = await saveReceipt(parsedData);
+
+      data = { notionURL, notionID, imageURL, fileKey, receiptData: { ...receiptData } };
+    } catch (e) {
+      error = e instanceof Error ? e.message : "An error occurred";
+    }
+
+    return { error, data };
   }),
 } satisfies FileRouter;
 
