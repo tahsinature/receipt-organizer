1a648203dfa73b3cd924183d5ba8fd2c9758e4f1
Tahsin <work.tahsin@gmail.com>
2024-06-08 16:16:44 -0400

    add drawer and picker for displaying result

--- src/components/CodeDrawer.tsx
+++ src/components/CodeDrawer.tsx
@@ -1,33 +1,21 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
 import { Drawer, DrawerClose, DrawerContent, DrawerDescription, DrawerFooter, DrawerHeader, DrawerTitle, DrawerTrigger } from "@/components/ui/drawer";
 import { JsonViewer } from "@textea/json-viewer";
 
 type CodeDrawerProps = {
   code: string;
+  isOpen: boolean;
+  onOpenChange: (isOpen: boolean) => void;
 };
 
 const CodeDrawer = (props: CodeDrawerProps) => {
   return (
-    <div>
-      <Drawer>
-        <DrawerTrigger>Open</DrawerTrigger>
-        <DrawerContent>
-          <DrawerHeader>
-            <DrawerTitle>Are you absolutely sure?</DrawerTitle>
-            <DrawerDescription>
-              <JsonViewer value={props.code} theme={"dark"} />
-            </DrawerDescription>
-          </DrawerHeader>
-          <DrawerFooter>
-            <DrawerClose>
-              <Button variant="secondary">Cancel</Button>
-            </DrawerClose>
-          </DrawerFooter>
-        </DrawerContent>
-      </Drawer>
-    </div>
+    <Drawer open={props.isOpen} onOpenChange={props.onOpenChange}>
+      <DrawerContent>
+        <JsonViewer rootName="localStorage" collapseStringsAfterLength={20} displayDataTypes={false} value={props.code} theme={"dark"} />
+      </DrawerContent>
+    </Drawer>
   );
 };
 
